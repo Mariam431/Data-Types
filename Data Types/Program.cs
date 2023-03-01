631708798f6767884bd3c378f2ee6122ab13a852
mariam <mariam@MYPROBOOK>
2023-03-01 17:19:09 +0400

    Add project files.

---
+++ Data Types/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Data_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+          int intType = 56870;
+          Console.WriteLine(intType);
+
+          short shortType = -1130;
+          Console.WriteLine(shortType);
+
+          byte byteType = 4;
+          Console.WriteLine(byteType);
+
+          long longType = 1453652736950;
+          Console.WriteLine(longType);
+
+          double doubleType = 3.14;
+          Console.WriteLine(doubleType);
+
+
+          uint uintType = 560;
+          Console.WriteLine(uintType);
+
+          ushort ushortType = 10;
+          Console.WriteLine(ushortType);
+
+          ulong ulongType = 5365273650;
+          Console.WriteLine(ulongType);
+
+          sbyte sbyteType = 4;
+          Console.WriteLine(sbyteType);
+
+        }
+    }
+}
